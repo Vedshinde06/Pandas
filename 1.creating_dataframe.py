788e8b7010c162aff41bd96bc81d3f1d3fa02741
Vedshinde06 <vedantshinde501@gmail.com>
2025-03-06 23:48:17 +0530

    code updated

--- 1.creating_dataframe.py
+++ 1.creating_dataframe.py
@@ -1,4 +1,10 @@
 import pandas as pd
+from typing import List
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     return pd.DataFrame(student_data, columns=['student_id','age'])
+
+
+data = [[1, 19], [2, 13], [3, 20]]
+df = createDataframe(data)
+print(df)
